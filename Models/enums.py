e88332e523b09fa7322b4dcfb70e17484eb3c20f
ThomasCodez <thomas.schaffrath@fim-rc.de>
2025-02-08 00:12:25 +0100

    some more methods for other pieces

--- Models/enums.py
+++ Models/enums.py
@@ -13,3 +13,16 @@ class BasicDice(Enum):
   curved_road = 4
   road_t_junction = 5
   
+class SpecialDice(Enum):
+  underground = 0
+  straight_train_station = 1
+  curved_train_station = 2
+
+class UniqueTiles(Enum):
+  four_rail = 0
+  three_rail_one_road = 1
+  two_rail_two_road_straight = 2
+  two_rail_two_road_curved = 3
+  one_rail_three_road = 4
+  four_road = 5
+  
--- Services/piece_services.py
+++ Services/piece_services.py
@@ -1,14 +1,37 @@
 import json
-from Models.enums import BasicDice
+from typing import List
+from Models.enums import BasicDice, SpecialDice
 from Models.piece import Piece
 
-def get_all_basic_pieces():
+def get_all_basic_pieces() -> List[Piece]:
   '''
   Returns all attainable pieces from the basic dice.
   '''
   with open("Services/basic_pieces.json") as file:
     return [Piece(**piece) for piece in json.load(file)]
   
-def get_basic_pieces_for_dice(diceResult: BasicDice):
-  #all_dice: List[Piece] = get_all_basic_pieces()
+def get_basic_pieces_for_dice(diceResult: BasicDice) -> List[Piece]:
+  '''
+  Returns all placable pieces for a given dice result
+  '''
   return [piece for piece in get_all_basic_pieces() if BasicDice[piece.name] == diceResult]
+
+def get_all_special_pieces() -> List[Piece]:
+  '''
+  Returns all placable "special" pieces, i.e the ones attainable by the fourth, different dice.
+  '''
+  with open("Services/special_pieces.json") as file:
+    return [Piece(**piece) for piece in json.load(file)]
+  
+def get_special_pieces_for_dice(diceResult: SpecialDice) -> List[Piece]:
+  '''
+  Returns all placable "special" pieces for a given dice result.
+  '''
+  return [piece for piece in get_all_special_pieces() if SpecialDice[piece.name] == diceResult]
+
+def get_all_unique_pieces() -> List[Piece]:
+  '''
+  Returns all placable "unique" pieces, i.e the ones not requiring a dice and that may only get placed once.
+  '''
+  with open("Services/unique_pieces.json") as file:
+    return [Piece(**piece) for piece in json.load(file)]
